b7c686e94b1e9c00ed581185e212a87f5b87c1d2
Aleksey Dorofeev <aleksei.dorofeev@steppechange.com>
2020-03-27 17:45:01 +0300

    measure time intervals

--- Fifo.hpp
+++ Fifo.hpp
@@ -4,10 +4,12 @@
 
 #pragma once
 
+#include <iostream>
+#include <sstream>
 #include <mutex>
 #include <thread>
 #include <array>
-#include <iostream>
+#include <chrono>
 
 // Защищенная в плане многопоточного доступа очередь типа FIFO с предварительным резервированием.
 // Организована по типу кольцевого буфера.
@@ -19,6 +21,7 @@ public:
         eGood = 0,
         eInterrupted
     };
+
     Fifo() : ix_read(0), ix_write(0), cur_size(0)
     {
     }
@@ -30,6 +33,7 @@ public:
         stopped_flag = true;
         cv_free.notify_all();
         cv.notify_all();
+
         std::this_thread::yield();
 
         std::unique_lock lock(mt);
@@ -42,12 +46,14 @@ public:
     }
 
     // Положить задачу в хвост очереди. Если очередь заполнена, поток будет заблокирован до появления свободного места.
-    // Ожидающий поток может быть разблокирован по внешнему сигналу, в этом случае задача не кладется в очередь и
-    // возвращается код eInterrupted.
+    // Ожидающий поток может быть разблокирован по внешнему сигналу прерывания работы, в этом случае задача не кладется
+    // в очередь и возвращается код eInterrupted.
     Ret_code push_back(Task const& request)
     {
         std::unique_lock lock(mt);
 
+        bool wait = cur_size == queue.max_size();
+        auto ts = std::chrono::steady_clock::now();
         while(cur_size == queue.max_size()) {
             lock.unlock();
             {
@@ -57,6 +63,11 @@ public:
                     return eInterrupted;
             }
             lock.lock();
+        }
+        if(wait) {
+            auto te = std::chrono::steady_clock::now();
+            auto dt = std::chrono::duration_cast<std::chrono::microseconds>(te - ts);
+            wait_for_free_space_us += dt;
         }
 
         if(stopped_flag)
@@ -68,10 +79,13 @@ public:
             ix_write = 0;
         ++cur_size;
 
+        ++push_tasks;
+
         if(cur_size == 1) {
             std::cout << "NOTIFY";
             cv.notify_one();
         }
+
         return eGood;
     }
 
@@ -82,16 +96,25 @@ public:
     {
         std::unique_lock lock(mt);
 
+        bool wait = cur_size == 0;
+        auto ts = std::chrono::steady_clock::now();
         while(cur_size == 0) {
             cv.wait(lock, [&] { return cur_size || stopped_flag; });
             if(stopped_flag)
                 return eInterrupted;
+        }
+        if(wait) {
+            auto te = std::chrono::steady_clock::now();
+            auto dt = std::chrono::duration_cast<std::chrono::microseconds>(te - ts);
+            wait_for_task_us += dt;
         }
 
         if(stopped_flag)
             return eInterrupted;
 
         std::cout << "POP" << ix_read;
+
+        ++pop_tasks;
 
         result = queue[ix_read];
         if(++ix_read == queue.max_size())
@@ -100,6 +123,13 @@ public:
             cv_free.notify_one();
         return eGood;
     }
+
+public:
+    inline static thread_local std::chrono::microseconds wait_for_free_space_us{0};
+    inline static thread_local uint64_t push_tasks{0};
+
+    inline static thread_local std::chrono::microseconds wait_for_task_us{0};
+    inline static thread_local uint64_t pop_tasks{0};
 
 private:
     std::atomic_bool stopped_flag;
@@ -115,5 +145,4 @@ private:
     size_t ix_write;
     size_t cur_size;
 };
-
 
--- main.cpp
+++ main.cpp
@@ -9,18 +9,21 @@
 
 enum {
     FIFO_SIZE = 32,
-    WORKER_THREAD_COUNT = 2,
-    EXECUTOR_THREAD_COUNT = 2,
+    WORKER_THREAD_COUNT = 1,
+    EXECUTOR_THREAD_COUNT = 1,
     ELEM_COUNT = 10,
-    RUN_COUNT = 32
+    RUN_COUNT = 32000
 };
 
 using Task = std::function<int ()>;
 using Fifo_type = Fifo<Task, FIFO_SIZE>;
 
-int sum(std::vector<int> numbers)
+// The simple function, that is used as Task for FIFO
+template <typename T>
+T sum(std::vector<T> numbers)
 {
-    return std::accumulate(numbers.begin(), numbers.end(), 0);
+    static_assert(std::is_constructible_v<T>);
+    return std::accumulate(numbers.begin(), numbers.end(), T(0));
 }
 
 void worker_routine(Fifo_type &fifo, size_t elem_count, size_t run_count)
@@ -28,27 +31,51 @@ void worker_routine(Fifo_type &fifo, size_t elem_count, size_t run_count)
     std::random_device rd;
     std::mt19937 g(rd());
 
+    auto ts = std::chrono::steady_clock::now();
+
     for(size_t i=0; i<run_count; ++i) {
         std::vector<int> numbers;
         for (size_t j=0; j<elem_count; ++j)
             numbers.push_back(g());
-        auto res = fifo.push_back(std::bind(&sum, std::move(numbers)));
+        auto res = fifo.push_back(std::bind(&sum<decltype(numbers)::value_type>, std::move(numbers)));
         if (res == Fifo_type::eInterrupted)
             break;
-        //std::this_thread::sleep_for(std::chrono::nanoseconds(200));
+        // эмулируем задержку до прихода следующей задачи
+        std::this_thread::sleep_for(std::chrono::nanoseconds(200));
     }
+
+    auto te = std::chrono::steady_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::microseconds>(te - ts);
+
+//    oss << std::endl
+//        << "worker[" << std::this_thread::get_id() << "]: "
+//        << "pushes " << fifo.push_tasks << " tasks"
+//        << ", run/wait for " << dt.count() << "/" << fifo.wait_for_free_space_us.count() << " us"
+//        << std::endl;
 }
 
 void executor_routine(Fifo_type &fifo)
 {
     Fifo_type::Ret_code res;
     Task task;
+
+    auto ts = std::chrono::steady_clock::now();
+
     while((res = fifo.pop_front(task)) != Fifo_type::eInterrupted) {
         if(task)
             int rc = task();
         // эмулируем тяжелую работу
         std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
     }
+
+    auto te = std::chrono::steady_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::microseconds>(te - ts);
+
+//    oss << std::endl
+//        << "executor[" << std::this_thread::get_id() << "]: "
+//        << "popes " << fifo.pop_tasks << " tasks"
+//        << ", run/wait for " << dt.count() << "/" << fifo.wait_for_task_us.count() << " us"
+//        << std::endl;
 }
 
 int main()
@@ -57,7 +84,8 @@ int main()
 
     Fifo_type fifo;
 
-    std::vector<std::thread> threads;
+    std::vector<std::thread> threads(EXECUTOR_THREAD_COUNT + WORKER_THREAD_COUNT);
+    std::vector<std::ostringstream> results(EXECUTOR_THREAD_COUNT + WORKER_THREAD_COUNT);
 
     for(size_t i = 0; i < EXECUTOR_THREAD_COUNT; ++i)
         threads.emplace_back(&executor_routine, std::ref(fifo));
@@ -69,8 +97,13 @@ int main()
     std::vector<Task> nft;
     fifo.stop(nft);
 
+    std::cout << "fifo has " << nft.size() << " non finished task(s)" << std::endl;
+
     for(auto &item: threads)
         item.join();
+
+    for(auto &item: results)
+        std::cout << item.str() << std::endl;
 
     return 0;
 }
